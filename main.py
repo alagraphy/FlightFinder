a94abffccbb5fae07b78b0943b72d754f5cb2c68
Jeremy J Hartmann <jhartman@sfu.ca>
2015-03-17 21:49:28 -0700

    Added simple csv writer

--- main.py
+++ main.py
@@ -5,15 +5,15 @@
 import csv
 import string
 from splinter import Browser
+import time
+import random
 
 
 # Start
 
-browser = Browser('chrome', '--incognito')
-
 #########################
 ## Fill flight numbers
-def fillflight(fieldnumber, frport, toport, date):
+def fillflight(browser, fieldnumber, frport, toport, date):
     # Fill in the first flight
     if fieldnumber < 2:
         browser.fill('FrAirport', frport)
@@ -27,18 +27,24 @@ def fillflight(fieldnumber, frport, toport, date):
 #########################
 ## Wrtie content to CSV
 def writetocsv(in_ofile, in_string):
-    splitstring = in_string.split('\n\n\n')
+    splitstring = in_string.split('\n\n\n\n')
     writer = csv.writer(in_ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+
+    index = in_string.find('C$');
+    writer.writerow([in_string[index:index+7]])
     for iter in splitstring:
         writer.writerow(iter.split('\n'))
 
+    writer.writerow("")
+    writer.writerow("")
 
-########################
-## MAIN FUNCTION
-def main():
-    # Vist google
+def VisitExpediaforFlights(in_ofile, in_first, in_second, in_third):
+    # Create Browser
+    browser = Browser('chrome', '--incognito')
+
+    # Visit google
     browser.visit('http://www.expedia.ca/Flights')
-    ofile = open("FilghtsAll.csv", 'wb')
+    time.sleep(10)
 
     # Choose multicity triip
     browser.choose('TripType', 'Multicity')
@@ -47,25 +53,50 @@ def main():
     browser.select('NumAdult', '3')
 
     # Fill in the first flight
-    fillflight(1, 'PEK', 'YVR', '06/08/2015')
+    fillflight(browser, 1, 'PEK', 'YVR', str(in_first) + '/08/2015')
 
     # Flight 2
-    fillflight(2, 'YVR', 'YSJ', '19/08/2015')
+    fillflight(browser, 2, 'YVR', 'YSJ', str(in_second) + '/08/2015')
 
     # Flight 3
-    fillflight(3, 'YSJ', 'PEK', '28/08/2015')
+    fillflight(browser, 3, 'YSJ', 'PEK', str(in_third) + '/08/2015')
 
     # Click on Search
     browser.find_by_id('F-searchButtonExt1').click()
+    time.sleep(10)
 
     # Parse the string
-    valuestring = browser.find_by_id('flightModule0').value
+    modulelist = browser.find_by_id('flightModuleList').value.split('SELECT')
 
-
+    #iterate over list
+    for moduleiter in modulelist:
+        writetocsv(in_ofile, moduleiter)
 
     # Close browser
     browser.quit()
+
+
+########################
+## MAIN FUNCTION
+def main():
+
+    # Open file for writing
+    ofile = open("FilghtsAll.csv", 'a')
+
+    firstday = 4
+    secondInterval = 14
+    thirdInterval = 25
+
+    for i in range(firstday, (firstday + 9)):
+        for k in range(secondInterval, secondInterval+6):
+            for m in range(thirdInterval, thirdInterval+7):
+                # Main Functin
+                VisitExpediaforFlights(ofile, i, k, m)
+                time.sleep(random.randint(45, 70))
+
+    #Close the ofile
     ofile.close()
+
 
 
 ## MAIN METHOD CALL
